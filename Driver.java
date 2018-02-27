edd30626382f6c376f030e06a71606b04f9ce175
ricortiz <ricortiz@iupui.edu>
2018-02-26 22:46:52 -0500

    Working driver file to simulate RoboRio iteration for a duration of 15 seconds (AutoMoide Duration) at a rate of 1 iteration every 20ms

--- Driver.java
+++ Driver.java
@@ -1,1 +1,14 @@
 //Driver.java will be utilized to test our code
+public class Driver
+{
+  public static void main(String[] args) throws InterruptedException
+    {
+      int iteration = 0;
+      while (iteration<750)
+      {
+        System.out.println("Iterated");
+        iteration++;
+        Thread.sleep(20);//sleep for 20ms
+      }
+    }
+}
