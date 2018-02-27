3371d5612106a64afc233b0c00ab2204c9fee3bc
ricortiz <ricortiz@iupui.edu>
2018-02-26 23:14:18 -0500

    Made improvements to code comments on Driver.java.

--- Driver.java
+++ Driver.java
@@ -1,14 +1,37 @@
 //Driver.java will be utilized to test our code
+//==============================================================================
+/**
+ * @file    Driver.java
+ * @date    2/26/2018
+ * @author  Cierra
+ * @author  Ricardo
+ *
+ */
+//==============================================================================
+/**
+ * @class Driver
+ *
+ * File utilized to test and simulate the implementation for CrossTheLine auto
+ */
 public class Driver
 {
   public static void main(String[] args) throws InterruptedException
     {
+      /// Functions shall be placed inside the while loop to simulate the
+      /// number of iterations and the iteration speed it will encounter
+      /// while running auto mode on RoboRio
+
+      /// The iteration variable will count the number of iterations that occur
+      /// in 15 seconds (750 at the 20ms per iteration rate of RoboRio)
       int iteration = 0;
       while (iteration<750)
       {
-        System.out.println("Iterated");
+        /// Place function to be tested between here...
+
+        /// ... and here!
         iteration++;
-        Thread.sleep(20);//sleep for 20ms
+        /// Sleep the thread for 20ms to simulate iteration time of RoboRio
+        Thread.sleep(20);
       }
     }
 }
