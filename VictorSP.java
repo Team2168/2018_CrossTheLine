67b151497457db6a31ba014d51c586646b2bbc38
Cierra <cc362x@gmail.com>
2018-02-28 18:44:55 -0500

    Added comments

--- VictorSP.java
+++ VictorSP.java
@@ -1,21 +1,28 @@
-
+//==============================================================================
+/**
+ * @file    VictorSP.java
+ * @date    2/27/2018
+ * @author  Cierra
+ * @author  Ricardo
+ * 
+ */
+//==============================================================================
+/**
+ * @class VictorSP
+ */
 public class VictorSP
 {
+	
 	
 	private int motorPin;
 	
 	VictorSP(int PWM) {
-		
 		this.motorPin = PWM;
-		
 	}
 
 	public void set(double speed) {
-		
 		System.out.format("The speed is %.2f and the pin is %d%n", speed, motorPin);
-		
 	}
-	
 
 	
 }
